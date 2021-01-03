6a72d195639a0fcada3bab0a114bb2798c59c0ff
mv-yurchenko <maxonus2@gmail.com>
2021-01-03 18:08:07 +0300

    Alembic started tables

---
+++ alembic/versions/825baed58510_tables_info.py
@@ -0,0 +1,36 @@
+"""tables_info
+
+Revision ID: 825baed58510
+Revises: af3c805654f0
+Create Date: 2021-01-01 20:57:59.375871
+
+"""
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "825baed58510"
+down_revision = "af3c805654f0"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "tables_info",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("table_name", sa.String(64), nullable=False),
+        sa.Column(
+            "folder_name", sa.String(64), nullable=False
+        ),  # something like schema
+        sa.Column("database_name", sa.String(), sa.ForeignKey("bases.name")),
+        sa.Column("local_name", sa.String(200), nullable=False),
+        sa.Column("columns", postgresql.JSON(), nullable=False),
+    )
+
+
+def downgrade():
+    op.drop_table("tables_info")
---
+++ alembic/versions/af3c805654f0_bases_table.py
@@ -0,0 +1,35 @@
+"""bases_table
+
+Revision ID: af3c805654f0
+Revises: 38936c9e50e0
+Create Date: 2021-01-01 20:02:32.814003
+
+"""
+import sqlalchemy as sa
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "af3c805654f0"
+down_revision = "38936c9e50e0"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "bases",
+        sa.Column("id", sa.Integer),
+        sa.Column("type", sa.String(64), nullable=False),
+        sa.Column("description", sa.String(200)),
+        sa.Column("name", sa.String(200), nullable=False, primary_key=True),
+        sa.Column("ip", sa.String(20), nullable=False),
+        sa.Column("port", sa.String(4), nullable=False),
+        sa.Column("username", sa.String(64), nullable=False),
+        sa.Column("password", sa.String(256), nullable=False),
+    )
+
+
+def downgrade():
+    op.drop_table("bases")
