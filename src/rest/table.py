d3a01a9e1947e12894350fc720d659aa1eefdb59
mv-yurchenko <maxonus2@gmail.com>
2021-03-30 09:05:56 +0300

    RB-15 Logging with loguru

--- src/rest/table.py
+++ src/rest/table.py
@@ -21,32 +21,61 @@ class Table(Resource):
         if not self.rest_helper.request_validator.validate_get_table_data_request(
             request
         ):
+            self.rest_helper.logger.log_incorrect_request("/Table/Get/", request)
             return make_response(*self.rest_helper.get_bad_request_answer())
 
         token = request.headers.get(USER_TOKEN_FIELD_NAME)
         table_name = request.args.get(LOCAL_TABLE_NAME_FILED_NAME)
         if token not in self.rest_helper.local_worker.get_tokens_list():
+            self.rest_helper.logger.log_not_found(
+                "/Table/Get", token, entity="token", entity_name=token
+            )
             return make_response("Token not found.", 404)
 
         if table_name not in self.rest_helper.local_worker.get_local_name_list():
+            self.rest_helper.logger.log_not_found(
+                "/Table/Get", token, entity="table", entity_name=table_name
+            )
             return make_response("Table not found.", 404)
 
         if table_name not in self.rest_helper.local_worker.get_token_tables(token):
+            self.rest_helper.logger.log_access_denied(
+                "/Table/Get", token, entity="table", entity_name=table_name
+            )
             return make_response("Access for token denied", 403)
 
-        return make_response(
-            {
-                "status": "success",
-                "TableData": self.rest_helper.local_worker.get_table_params(table_name),
-            }
-        )
+        try:
+            table_data = self.rest_helper.local_worker.get_table_params(table_name)
+            self.rest_helper.logger.log_status_execution(
+                "/Table/Get/", token, "success", request
+            )
+            return make_response(
+                {
+                    "status": "success",
+                    "TableData": table_data,
+                },
+                200,
+            )
+        except Exception:
+            self.rest_helper.logger.logger.exception("Error.")
+            self.rest_helper.logger.log_status_execution(
+                "/Table/Get/", token, "error", request
+            )
+            return make_response({"status": "Unexpected error"}, 400)
 
     def post(self):
         # Function to change local table name
         if not self.rest_helper.request_validator.validate_set_local_table_name_request(
             request
         ):
+            self.rest_helper.logger.log_incorrect_request("/Table/Post/", request)
             return make_response(*self.rest_helper.get_bad_request_answer())
+
+        token = request.headers.get(ADMIN_TOKEN_FIELD_NAME)
+
+        if not self.rest_helper.token_worker.is_token_admin(token):
+            self.rest_helper.logger.log_access_denied("/Table/Post/", token)
+            return make_response("Access denied", 403)
 
         # Check if path to old table is correct
         if LOCAL_TABLE_NAME_FILED_NAME not in request.get_json()[TABLE_NAME_FIELD_NAME]:
@@ -63,31 +92,59 @@ class Table(Resource):
             )
 
         if local_table_name not in self.rest_helper.local_worker.get_local_name_list():
+            self.rest_helper.logger.log_not_found(
+                "/Table/Post/", token, entity="table", entity_name=local_table_name
+            )
             return make_response("Table not found", 404)
 
         if (
             request.get_json().get(NEW_LOCAL_TABLE_NAME_FILED_NAME)
             in self.rest_helper.local_worker.get_local_name_list()
         ):
+            self.rest_helper.logger.log_status_execution(
+                "/Table/Post",
+                token,
+                "error",
+                request,
+                failure_reason=f"Table with local name: '{request.get_json().get(NEW_LOCAL_TABLE_NAME_FILED_NAME)}' "
+                f"already exists",
+            )
             return make_response(
                 f"Table with local name: '{request.get_json().get(NEW_LOCAL_TABLE_NAME_FILED_NAME)}' already exists",
                 409,
             )
 
-        self.rest_helper.local_worker.change_local_name(
-            "table",
-            local_table_name,
-            request.get_json().get(NEW_LOCAL_TABLE_NAME_FILED_NAME),
-        )
+        try:
+            self.rest_helper.local_worker.change_local_name(
+                "table",
+                local_table_name,
+                request.get_json().get(NEW_LOCAL_TABLE_NAME_FILED_NAME),
+            )
 
-        return make_response(
-            {
-                "status": "success",
-                "new_table_name": request.get_json().get(
-                    NEW_LOCAL_TABLE_NAME_FILED_NAME
-                ),
-            }
-        )
+            self.rest_helper.logger.info(
+                f"Successfully renamed {local_table_name} -> "
+                f"{request.get_json().get(NEW_LOCAL_TABLE_NAME_FILED_NAME)}"
+            )
+
+            return make_response(
+                {
+                    "status": "success",
+                    "new_table_name": request.get_json().get(
+                        NEW_LOCAL_TABLE_NAME_FILED_NAME
+                    ),
+                },
+                200,
+            )
+        except Exception as e:
+            self.rest_helper.logger.log_status_execution(
+                "/Table/Post", token, "error", request, failure_reason=str(e)
+            )
+            return make_response(
+                {
+                    "status": "Unexpected error",
+                },
+                400,
+            )
 
 
 class ListTable(Resource):
@@ -97,6 +154,7 @@ class ListTable(Resource):
 
     def get(self):
         if not self.rest_helper.request_validator.validate_list_tables_request(request):
+            self.rest_helper.logger.log_incorrect_request("/ListTable/Get/", request)
             return make_response(*self.rest_helper.get_bad_request_answer())
 
         if ADMIN_TOKEN_FIELD_NAME in request.headers:
@@ -105,11 +163,31 @@ class ListTable(Resource):
             token = request.headers.get(USER_TOKEN_FIELD_NAME)
 
         if token not in self.rest_helper.local_worker.get_tokens_list():
+            self.rest_helper.logger.log_not_found(
+                "/ListTable/Get", token, entity="token", entity_name=token
+            )
             return make_response("Token not found.", 404)
 
-        return make_response(
-            {
-                "status": "success",
-                "List": self.rest_helper.local_worker.get_token_tables(token),
-            }
-        )
+        try:
+            table_list = self.rest_helper.local_worker.get_token_tables(token)
+            self.rest_helper.logger.log_status_execution(
+                "/ListTable/Get/", token, "success", request
+            )
+            return make_response(
+                {
+                    "status": "success",
+                    "List": table_list,
+                },
+                200,
+            )
+
+        except Exception as e:
+            self.rest_helper.logger.log_status_execution(
+                "/ListTable/Get/", token, "error", request, failure_reason=str(e)
+            )
+            return make_response(
+                {
+                    "status": "error",
+                },
+                400,
+            )
