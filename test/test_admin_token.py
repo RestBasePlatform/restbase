a83f9e72d82344caf5e405d734ec65b8608571b2
mv-yurchenko <maxonus2@gmail.com>
2021-05-11 21:53:26 +0300

    tests

--- test/test_admin_token.py
+++ test/test_admin_token.py
@@ -4,7 +4,7 @@ import json
 
 
 def test_generate_admin_token(restbase_url, test_main_admin_token):
-    response = requests.post(restbase_url + "/AdminToken/Generate/",
+    response = requests.post(restbase_url + "AdminToken/Generate/",
                              headers={"token": test_main_admin_token},
                              data=json.dumps({"token_name": "test_token"}))
     assert response.status_code == 200
