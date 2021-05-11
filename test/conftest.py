79b2f0d4853d2212685a13c9240bb97064ee7f50
mv-yurchenko <maxonus2@gmail.com>
2021-05-11 22:24:13 +0300

    precommit

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../source/")))
 
 
 @pytest.fixture(scope="session")
@@ -9,3 +16,16 @@ def test_main_admin_token() -> str:
 @pytest.fixture(scope="session")
 def restbase_url():
     return "http://localhost:54541/"
+
+
+@pytest.fixture(scope="session")
+def postgres_engine():
+    db_str = "postgresql://postgres:password@internal_postgres/postgres"
+    engine = create_engine(db_str)
+    return engine
+
+
+@pytest.fixture(scope="session")
+def postgres_session(postgres_engine):
+    session = Session(bind=postgres_engine)
+    return session
--- test/test_admin_token.py
+++ test/test_admin_token.py
@@ -1,12 +1,23 @@
 import json
 
 import requests
+from utils import get_existing_data
+from tables import TokenTable
 
 
-def test_generate_admin_token(restbase_url, test_main_admin_token):
+def test_generate_admin_token(restbase_url, test_main_admin_token, postgres_session):
     response = requests.post(
         restbase_url + "AdminToken/Generate/",
         headers={"token": test_main_admin_token},
         data=json.dumps({"token_name": "test_token"}),
     )
     assert response.status_code == 200
+    new_token = response.text
+
+    tokens = get_existing_data(
+        postgres_session,
+        TokenTable,
+    )
+
+    # Check new token exists in database
+    assert new_token in [i.token for i in tokens if i.admin_access]
