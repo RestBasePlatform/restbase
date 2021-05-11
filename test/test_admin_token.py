b89d41f5e6b480c783ab7dc1e1bbb7d992d1f9c9
mv-yurchenko <maxonus2@gmail.com>
2021-05-11 22:50:00 +0300

    tests

--- test/test_admin_token.py
+++ test/test_admin_token.py
@@ -11,8 +11,7 @@ from utils import get_existing_data
     "data_dict,expected_status_code",
     [
         ({"token_name": "test_token"}, 200),
-        ({"token_name": "test_token", "description": "test_description"}, 200),
-        ({}, 422),
+        ({"token_name": "test_token", "description": "test_description"}, 200)
     ],
 )
 def test_generate_admin_token_correct_header(
@@ -37,27 +36,27 @@ def test_generate_admin_token_correct_header(
 
     # Check new token exists in database
     assert new_token in [i.token for i in tokens if i.admin_access]
-    if data_dict.get('description'):
-        assert data_dict.get('description') in [i.description for i in tokens if i.token == new_token]
+    if data_dict.get("description"):
+        assert data_dict.get("description") in [
+            i.description for i in tokens if i.token == new_token
+        ]
 
 
 @pytest.mark.parametrize(
     "data_dict,expected_status_code",
     [
         ({"token_name": "test_token"}, 403),
-        ({"token_name": "test_token", "description": "test_description"}, 403),
-        ({}, 403),
+        ({"token_name": "test_token", "description": "test_description"}, 403)
     ],
 )
 def test_generate_admin_token_incorrect_header(
     restbase_url,
-    test_main_admin_token,
     data_dict,
     expected_status_code,
 ):
     response = requests.post(
         restbase_url + "AdminToken/Generate/",
-        headers={"token": test_main_admin_token},
+        headers={"token": "123"},
         data=json.dumps(data_dict),
     )
     assert response.status_code == expected_status_code
